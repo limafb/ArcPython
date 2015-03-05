c938632d16f2587a60fc0074ac80640a8ec42fc3
limafb <buenofb@hotmail.com>
2015-03-05 20:47:02 -0300

    Update MultipleRingBuffer
    Pequenos bugs corrigidos

--- MultipleRingBuffer.py
+++ MultipleRingBuffer.py
@@ -1,18 +1,17 @@
 def MultipleRingBuffer(feat_class, r_ini, r_inc, qnt_buff):
-    import arcpy 			#le todas as categorias do arcpy
+    import arcpy 			
     from arcpy import *
-    arcpy.env.workspace = "D://Cursos//LabGIS//Python//MaterialDidatico//praticas//dadosP03//teste"
     raio = r_ini
-    listSHP = []
+    listFeature = []
     for i in range(qnt_buff):
         Buffer_analysis(in_features=feat_class,
-	out_feature_class="test"+str(i)+".shp", 
+	out_feature_class="buffer"+str(i), 
 	buffer_distance_or_field=raio, line_side="FULL",
 	line_end_type="ROUND", dissolve_option="NONE", dissolve_field="",
 	method="PLANAR")
+        listFeature.append("buffer"+str(i))
 	raio += r_inc
-    listSHP = arcpy.ListFeatureClasses('*.shp')
-    listSHP.sort(reverse=True)
-    arcpy.Merge_management(listSHP, "teste_merge.shp")
-    for shp in listSHP:
-        Delete_management(shp)
+    listFeature.sort(reverse=True)
+    arcpy.Merge_management(listFeature, "merge"+feat_class)
+    for feature in listFeature:
+        Delete_management(env.workspace+str("/")+feature)
