397ddf4533833c75cae3d1677c6999acb359d481
limafb <buenofb@hotmail.com>
2015-03-07 16:06:55 -0300

    Avaliacao3.py updated
    Correção de pequenos bugs

--- Avaliacao3/Avaliacao3.py
+++ Avaliacao3/Avaliacao3.py
@@ -1,41 +1,49 @@
 # habilita a extensao Spatial Analyst e carrega a biblioteca
 arcpy.CheckOutExtension("Spatial")
 from arcpy.sa import *
+import os
+
 # pega os parametros informados pelo usuario na janela da ferramenta
 # pega a pasta de entrada com os arquivos a serem cortados
 inputPath = arcpy.GetParameterAsText(0)
-inputPath = "C:/Users/FabioLap/Documents/GitHub/ArcPython/Avaliacao3/Dados_Avaliacao_3/Projeto_1"
 
 # pega um Shapefile do tipo polígono para máscara de corte
 clipFeature = arcpy.GetParameterAsText(1)
-clipFeature = "C:/Users/FabioLap/Documents/GitHub/ArcPython/Avaliacao3/Dados_Avaliacao_3/Projeto_1/MASCARA.shp"
+
+# testa se a mascara de entrada é do tipo polígono
+if not (arcpy.Describe(clipFeature)).shapeType == "Polygon":
+    arcpy.AddWarning("A máscara deve ser um polígono")
 
 # pega a pasta onde os arquivos resultantes serão salvos
-outputPath = arcpy.GetParameterAsText(4)
-outputPath = "C:/Users/FabioLap/Documents/GitHub/ArcPython/Avaliacao3/Dados_Avaliacao_3/Projeto_1/Resultado"
+outputPath = arcpy.GetParameterAsText(2)
+
+# testa se a pasta de saída é igual a pasta de entrada
+if inputPath == outputPath:
+    arcpy.AddWarning("A pasta de saída deve ser diferente da pasta de entrada")
 
 # pega os arquivos e corta com a mascara fornecida
-Datasets = arcpy.ListDatasets(inputPath)
-Datasets = arcpy.ListDatasets()
-Rasters = arcpy.ListRasters()
-Features = arcpy.ListFeatureClasses()
-for fc in arcpy.ListFeatureClasses():
+# define o ambiente como a pasta de entrada
+arcpy.env.workspace = inputPath
 
-if not arcpy.ListFeatureClasses():
-    print "Não ha feature class"
+# testa se há Feature Classes na pasta de entrada, em caso
+# positivo executa o clip
+if not arcpy.ListFeatureClasses('*.shp'):
+    arcpy.AddWarning("Nao ha arquivos shape em" + inputPath)
 else:
     for fc in arcpy.ListFeatureClasses():
-        # Set the output name to be the same as the input name, and
-        #    locate in the 'out_workspace' workspace
+        # Define o nome de saída como o mesmo do de entrada
+        # e define a pasta de saída
         #
         output = os.path.join(outputPath, fc)
 
         # Clip each input feature class in the list
         #
-        arcpy.Clip_analysis(fc, clipFeature, output, 0.1)
+        arcpy.Clip_analysis(fc, clipFeature, output, 0.001)
 
-if not arcpy.ListRasters():
-    print "Não há arquivos raster"
+# testa se há arquivos raster na pasta de entrada, em caso
+# positivo executa o extract by mask
+if not arcpy.ListRasters('*.tif'):
+    arcpy.AddWarning("Nao ha arquivos tif em" + inputPath)
 else:
     for rs in arcpy.ListRasters():
         # Set the output name to be the same as the input name, and
@@ -45,7 +53,7 @@ else:
 
         # Extract each input raster in the list
         #
-        extract = ExtractByMask(rs, output)
+        extract = ExtractByMask(rs, clipFeature)
         
         # salva o raster resultante no caminho indicado pelo usuario
-        extract.save(outputPath)
+        extract.save(output)
